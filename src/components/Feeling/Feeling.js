7fbc911fee755483266136f24069349f0b5164ed
Mike Lynch <mjlynch2@gmail.com>
2019-09-27 16:54:28 -0500

    add radio buttons to Feeling component

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -7,10 +7,25 @@ class Feeling extends Component {
         this.props.history.push('/understanding');
     }
 
+    handleChange = (event) => {
+        console.log(event.target.value);
+        
+    }
+
     render(){
         return (
             <div>
                 <h2>How are you feeling today?</h2>
+                <span> 
+                    I'm very stressed.
+                    <input type="radio" name="type" value="1" onChange={(event) => this.handleChange(event)} />
+                    <input type="radio" name="type" value="2" onChange={(event) => this.handleChange(event)} />
+                    <input type="radio" name="type" value="3" onChange={(event) => this.handleChange(event)} />
+                    <input type="radio" name="type" value="4" onChange={(event) => this.handleChange(event)} />
+                    <input type="radio" name="type" value="5" onChange={(event) => this.handleChange(event)} />
+                    I'm feeling great!
+                </span>
+                <br/>
                 <button id="nextButton" onClick={this.handleClick}>Next</button>
             </div>
         )

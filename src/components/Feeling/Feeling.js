1ea6bcceec66e25efaacfb21eea652b4e58f8b8e
Mike Lynch <mjlynch2@gmail.com>
2019-09-28 08:21:40 -0500

    build Understanding component: select 1-5, handleChange

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+// DESCRIPTIVE COMMENT
 class Feeling extends Component {
 
     state = {
--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -7,10 +7,22 @@ class Understanding extends Component {
         this.props.history.push('/support');
     }
 
+    handleChange = (event) => {
+        // this.setState({ isValid: true });
+        this.props.dispatch({ type: 'SET_FEEDBACK', payload: event.target.value, keyName: 'understanding' })
+    }
+
     render() {
         return (
             <div>
                 <h2>How well are you understanding the content?</h2>
+                <select onChange={this.handleChange}>
+                    <option value="1">1 - I'm totally lost</option>
+                    <option value="2">2</option>
+                    <option value="3">3 - I'm getting there</option>
+                    <option value="4">4</option>
+                    <option value="5">5 - I've got this!</option>
+                </select>
                 <button id="nextButton" onClick={this.handleClick}>Next</button>
             </div>        
         )

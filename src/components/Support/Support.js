1ab1205f8958ac665f6a69aa586ce678629e31be
Mike Lynch <mjlynch2@gmail.com>
2019-09-28 08:25:04 -0500

    build Supoort component: select 1-5, add handleChange, dispatch to redux store

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -7,10 +7,22 @@ class Support extends Component {
         this.props.history.push('/comment');
     }
 
+    handleChange = (event) => {
+        // this.setState({ isValid: true });
+        this.props.dispatch({ type: 'SET_FEEDBACK', payload: event.target.value, keyName: 'support' })
+    }
+
     render() {
         return (
             <div>
                 <h2>How well are you being supported?</h2>
+                <select onChange={this.handleChange}>
+                    <option value="1">1 - I feel abandoned</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5 - I feel supported!</option>
+                </select>
                 <button id="nextButton" onClick={this.handleClick}>Next</button>
             </div>
         )

58056439c2938d4a831d28422f85a59e64a26be4
Mike Lynch <mjlynch2@gmail.com>
2019-09-28 11:07:14 -0500

    build Comment component: textare to add a comment, add handleChange, dispatch to redux store

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Comment extends Component {
+    state = {
+        comment: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({comment: event.target.value})
+    }
 
     handleClick = () => {
+        this.props.dispatch({type: 'SET_FEEDBACK', payload: this.state.comment, keyName: 'comment'})
         this.props.history.push('/review');
     }
 
@@ -11,6 +19,7 @@ class Comment extends Component {
         return (
             <div>
                 <h2>Any comments you want to leave?</h2>
+                <textarea onChange={this.handleChange} />
                 <button id="nextButton" onClick={this.handleClick}>Next</button>
             </div>
         )

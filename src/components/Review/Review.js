576b15d21e5fd0e4684047129373c0367ba45f78
Mike Lynch <mjlynch2@gmail.com>
2019-09-28 11:13:35 -0500

    build Review component: get the feedback values from the redux store and display them on the dom

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -8,9 +8,14 @@ class Review extends Component {
     }
 
     render() {
+        const { feeling, understanding, support, comment } = this.props.reduxState.feedbackReducer;
         return (
             <div>
                 <h2>Review Your Feedback</h2>
+                <p>Feeling: {feeling}</p>
+                <p>Understanding: {understanding}</p>
+                <p>Support: {support}</p>
+                <p>Comment: {comment}</p>
                 <button id="nextButton" onClick={this.handleClick}>Submit</button>
             </div>
         )

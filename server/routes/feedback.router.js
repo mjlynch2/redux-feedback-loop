3fd30bf1878116596ab6f3040d750563d2b08e7a
Mike Lynch <mjlynch2@gmail.com>
2019-09-29 09:42:14 -0500

    stretch: build server-side GET route to select all columns from the feedback table, ordered by date

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+router.get('/', (req, res) => {
+    const query = `SELECT * FROM "feedback" ORDER BY "date" DESC`;
+    pool.query(query)
+        .then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log('Error in server POST:', error);
+        })
+})
+
 router.post('/', (req, res) => {
     const values = [req.body.feeling, req.body.understanding, req.body.support, req.body.comments];
     const query = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4)`;
